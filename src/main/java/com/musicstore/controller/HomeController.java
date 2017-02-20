39bf9125acba70d7b0aca7aeaaaa34c7771473fe
Naveen Patil <naveenpatilm@gmail.com>
2017-02-20 15:20:14 +0530

    Multiple product listing and navigate between home and products page, make use of bootstrap

--- src/main/java/com/musicstore/controller/HomeController.java
+++ src/main/java/com/musicstore/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.musicstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +15,17 @@ public class HomeController {
 
 	@Autowired
 	private ProductDao productDao;
-	
-    @RequestMapping("/")
-    public String home() {
-        return "home";
-    }
-     
-    @RequestMapping("/productList")
-    public String getProducts(Model model) {
-    	Product product = productDao.getAllProducts().get(0);
-    	model.addAttribute(product);
-    	
-    	return "productList";
-    }
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("/productList")
+	public String getProducts(Model model) {
+		List<Product> products = productDao.getAllProducts();
+		model.addAttribute("products", products);
+
+		return "productList";
+	}
 }
--- src/main/java/com/musicstore/dao/ProductDao.java
+++ src/main/java/com/musicstore/dao/ProductDao.java
@@ -10,7 +10,7 @@ import com.musicstore.model.Product;
 @Component
 public class ProductDao {
 	private List<Product> productList;
-	
+
 	public List<Product> getAllProducts() {
 		Product product = new Product();
 		product.setProductName("4 Hour Work Week");
@@ -19,10 +19,19 @@ public class ProductDao {
 		product.setProductManufacturer("ferris");
 		product.setProductPrice(300);
 		product.setUnitInStock(109);
-		
+
+		Product product2 = new Product();
+		product2.setProductName("Richie Rich");
+		product2.setProductCategory("Book");
+		product2.setProductCondition("new");
+		product2.setProductManufacturer("unknown");
+		product2.setProductPrice(99);
+		product2.setUnitInStock(3);
+
 		productList = new ArrayList<Product>();
 		productList.add(product);
-		
+		productList.add(product2);
+
 		return productList;
 	}
 }
