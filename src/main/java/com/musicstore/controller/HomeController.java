63390ea8aaf28e8ae1f2018e7218a9892497fd0f
Naveen Patil <naveenpatilm@gmail.com>
2017-02-24 23:56:31 +0530

    Add product setup

--- src/main/java/com/musicstore/controller/HomeController.java
+++ src/main/java/com/musicstore/controller/HomeController.java
@@ -37,4 +37,26 @@ public class HomeController {
 		model.addAttribute("product", product);
 		return "viewProduct";
 	}
+
+	@RequestMapping("/admin")
+	public String adminPage() {
+		return "admin";
+	}
+
+	@RequestMapping("/admin/productInventory")
+	public String productInventory(Model model) {
+		List<Product> products = productDao.getAllProducts();
+		model.addAttribute("products", products);
+		return "productInventory";
+	}
+
+	@RequestMapping("/admin/productInventory/addProduct")
+	public String addProduct(Model model) {
+		Product product = new Product();
+		product.setProductCategory("Instrument");
+		product.setProductCondition("new");
+		product.setProductStatus("availaible");
+		model.addAttribute("product", product);
+		return "addProduct";
+	}
 }
