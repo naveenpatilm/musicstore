4e5a274addd82af1d1d83da8919f0020c1784b0d
Naveen Patil <naveenpatilm@gmail.com>
2017-03-01 09:48:33 +0530

    Intermediate

--- src/main/java/com/musicstore/controller/HomeController.java
+++ src/main/java/com/musicstore/controller/HomeController.java
@@ -1,21 +1,31 @@
 package com.musicstore.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
+import javax.management.RuntimeErrorException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.musicstore.dao.ProductDao;
 import com.musicstore.model.Product;
 
 @Controller
 public class HomeController {
+	
+	private Path path;
 
 	@Autowired
 	private ProductDao productDao;
@@ -59,8 +69,19 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
-	public String addProductPost(@ModelAttribute("product") Product product) {
+	public String addProductPost(@ModelAttribute("product") Product product, HttpServletRequest httpServletRequest) {
 		productDao.addProduct(product);
+		MultipartFile productImage = product.getProductImage();
+		String rootDirectory = httpServletRequest.getSession().getServletContext().getRealPath("/");
+		path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\images\\" + product.getProductId() + ".png");
+		if(productImage != null && !productImage.isEmpty()) {
+			try {
+				productImage.transferTo(new File(path.toString()));
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("Product image saving failed");
+			}
+		}
 		return "redirect:/admin/productInventory";
 
 	}
